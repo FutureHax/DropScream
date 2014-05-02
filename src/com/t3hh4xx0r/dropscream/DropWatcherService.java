1052e65439abc7c62ab06a101a29a09be804ca84
r2doesinc <r2doesinc@gmail.com>
2014-05-02 17:10:11 -0400

    play audio

--- src/com/t3hh4xx0r/dropscream/DropWatcherService.java
+++ src/com/t3hh4xx0r/dropscream/DropWatcherService.java
@@ -3,6 +3,8 @@ package com.t3hh4xx0r.dropscream;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -11,6 +13,7 @@ import com.t3hh4xx0r.dropscream.AccelerometerManager.AccelerometerListener;
 public class DropWatcherService extends Service implements
 		AccelerometerListener {
 	int dropCount = 0;
+	MediaPlayer m;
 
 	public DropWatcherService() {
 	}
@@ -36,6 +39,8 @@ public class DropWatcherService extends Service implements
 		if (AccelerometerManager.isSupported(this)) {
 			AccelerometerManager.startListening(this);
 		}
+		m = new MediaPlayer();
+		prepareScream();
 		return super.onStartCommand(intent, flags, startId);
 	}
 
@@ -47,12 +52,49 @@ public class DropWatcherService extends Service implements
 	@Override
 	public void onDropStarted() {
 		dropCount = dropCount + 1;
-
+		playScream();
 		Toast.makeText(this, "DROP STARTED", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public void onDropStopped() {
 		Toast.makeText(this, "DROPPED " + dropCount, Toast.LENGTH_SHORT).show();
+	}
+
+	public void stopScream() {
+		try {
+			if (m.isPlaying()) {
+				m.stop();
+				m.release();
+			}
+		} catch (Exception e) {
+
+		}
+	}
+
+	public void prepareScream() {
+		try {
+			if (m.isPlaying()) {
+				m.stop();
+				m.release();
+				m = new MediaPlayer();
+			}
+
+			AssetFileDescriptor descriptor = getAssets().openFd(
+					"wilhelm_scream.mp3");
+			m.setDataSource(descriptor.getFileDescriptor(),
+					descriptor.getStartOffset(), descriptor.getLength());
+			descriptor.close();
+
+			m.prepare();
+			m.setVolume(1f, 1f);
+			m.setLooping(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void playScream() {
+		m.start();
 	}
 }
